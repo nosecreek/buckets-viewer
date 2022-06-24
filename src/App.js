eefcc9f3246f98a6872be1ea5917ec6d07024a28
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-06-24 14:22:15 -0600

    Pick a file and load it into the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import  { useEffect, useState } from 'react';
+import useDrivePicker from 'react-google-drive-picker'
+import axios from 'axios'
+
 
 function App() {
+  const [openPicker, authResponse] = useDrivePicker();  
+  const [fileId, setFileId] = useState(null)  
+  // const customViewsArray = [new google.picker.DocsView()]; // custom view
+  const handleOpenPicker = () => {
+    openPicker({
+      clientId: "758483264841-9kmp9h0v8qghfn8e7d4k5mjg99d0fpn8.apps.googleusercontent.com",
+      developerKey: "AIzaSyAmKw4RxJhq67-H-M2HkjoqibzJN5fzqds",
+      viewId: "DOCS",
+      // token: localStorage.getItem('auth') || null, // pass oauth token in case you already have one
+      showUploadView: true,
+      showUploadFolders: true,
+      supportDrives: true,
+      multiselect: true,
+      customScopes: ['https://www.googleapis.com/auth/drive.file'],
+      viewMimeTypes: "application/octet-stream",
+      // customViews: customViewsArray, // custom view
+      callbackFunction: (data) => {
+        if (data.action === 'cancel') {
+          console.log('User clicked cancel/close button')
+        }
+        if(data.action === 'picked') {
+          console.log(data)
+          setFileId(data.docs[0].id)
+          localStorage.setItem('fileId', data.docs[0].id);
+        }
+      },
+    })
+  }
+
+  useEffect(()=>{
+    if(authResponse && fileId) {
+      const getFile = async () => {
+        const config =  {
+          headers: { Authorization: `Bearer ${authResponse.access_token}` }
+        }
+        const response = await axios.get(`https://www.googleapis.com/drive/v3/files/${fileId}?alt=media`, config)
+        console.log(response)
+      }
+      console.log(authResponse, fileId)
+      localStorage.setItem('auth', authResponse.access_token);
+      getFile()
+    }
+    console.log(fileId, authResponse)
+  }, [authResponse, fileId])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <button onClick={() => handleOpenPicker()}>Open Picker</button>
     </div>
   );
 }
