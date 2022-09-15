e6336ce4a53c4fdb835d7370c5352b6245021264
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-09-15 13:03:21 -0600

    Add tests for Accounts component

--- src/components/Accounts.test.js
+++ src/components/Accounts.test.js
@@ -2,27 +2,22 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import Buckets from './Buckets'
+import Accounts from './Accounts'
 
-describe('Buckets component displays buckets correctly', () => {
+describe('Accounts component displays accounts correctly', () => {
   let container
   const setTransactionView = jest.fn()
 
   beforeEach(() => {
-    const buckets = new Map([[1,[[-1,"Buckets License",500,1],[1,"Rent",0,1],[2,"Groceries",-25125,1],[3,"Car",59750,1]]],[-1,[[0,"Hobbies",47729,-1],[4,"Activities",33680,-1]]]])
-    const bucketCats = {
-      columns: ['id', 'name'],
-      values: [[1, 'Living Expenses']]
-    }
+    const accounts = [[1,"Savings",465000,""],[2,"Credit Card",-68966,"debt"],[3,"Chequing",5000,""],[4,"Long-Term Savings",2105000,"offbudget"]]
     const currency = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD'
     })
 
     container = render(
-      <Buckets
-        buckets={buckets}
-        bucketCats={bucketCats}
+      <Accounts
+        accounts={accounts}
         setTransactionView={setTransactionView}
         currency={currency}
       />
@@ -31,29 +26,29 @@ describe('Buckets component displays buckets correctly', () => {
 
   test('renders accordion header', () => {
     const element = container.querySelector('.accordion-header button')
-    expect(element).toHaveTextContent('Living Expenses')
+    expect(element).toHaveTextContent('Budget Accounts')
   })
 
-  test('renders list of buckets', () => {
+  test('renders list of accounts', () => {
     const element = container.querySelector('.accordion-item td')
-    expect(element).toHaveTextContent('Buckets License')
+    expect(element).toHaveTextContent('Savings')
   })
 
-  test('renders bucket balances', () => {
+  test('renders account balances', () => {
     const element = container.querySelector('.accordion-item td:nth-child(2)')
-    expect(element).toHaveTextContent('$5.00')
+    expect(element).toHaveTextContent('$4,650.00')
   })
 
-  test('clicking a bucket calls handler to change view', async () => {
+  test('clicking an account calls handler to change view', async () => {
     const user = userEvent.setup()
-    const element = screen.getByText('Groceries')
+    const element = screen.getByText('Chequing')
     await user.click(element)
 
     expect(setTransactionView).toHaveBeenCalledWith([
-      'bucket',
-      2,
-      'Groceries',
-      -25125
+      'account',
+      3,
+      'Chequing',
+      5000
     ])
     expect(setTransactionView).toHaveBeenCalledTimes(1)
   })
